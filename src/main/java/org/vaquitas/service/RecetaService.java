61d3c1962159cd0772f2a8fda767a1b95cf10b04
minmyn <elpezbanan0muak@gmail.com>
2025-11-25 20:58:07 -0600

    Enhance guardarReceta method in RecetaService
    Refactor guardarReceta method to improve clarity and efficiency in handling recordatorio.

--- src/main/java/org/vaquitas/service/RecetaService.java
+++ src/main/java/org/vaquitas/service/RecetaService.java
@@ -27,7 +27,6 @@ public class RecetaService {
         this.consultaRepository = consultaRepository;
     }
 
-    // ...
     public void guardarReceta(Receta receta) throws SQLException {
 
         try (Connection connection = DatabaseConfig.getDataSource().getConnection()) {
@@ -36,22 +35,16 @@ public class RecetaService {
                 Consulta consulta = receta.getConsulta();
                 int idConsulta = consultaRepository.save(consulta);
 
-                // 2. Manejar Recordatorio
                 int idRecordatorio;
-                // Guardamos una referencia al objeto original (el que viene en el JSON)
                 Recordatorio recordatorioASalvar = receta.getRecordatorio();
-                // Buscamos si ya existe en la base de datos
                 Recordatorio recordatorioEncontrado = recordatorioRepository.search(recordatorioASalvar);
 
                 if (recordatorioEncontrado == null) {
-                    // Si NO existe, lo guardamos y obtenemos su ID
                     idRecordatorio = recordatorioRepository.save(recordatorioASalvar);
                 } else {
-                    // Si SÍ existe, usamos el ID del encontrado
                     idRecordatorio = recordatorioEncontrado.getIdRecordatorio();
                 }
 
-                // 3. Guardar Receta
                 recetaRepository.save(receta, idConsulta, idRecordatorio);
 
                 connection.commit();
@@ -63,7 +56,7 @@ public class RecetaService {
             }
         }
     }
-// ...
+
 
     public List<Receta> verRecetas() throws SQLException {
         return recetaRepository.findAll();
